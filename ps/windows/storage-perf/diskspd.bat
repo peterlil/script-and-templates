d348eecc7b0a86179a32d0be6f5621c923873a3a
Peter Liljenroth <sail@live.se>
2019-03-21 13:14:15 +0100

    adding iops tests

--- ps/windows/storage-perf/diskspd.bat
+++ ps/windows/storage-perf/diskspd.bat
@@ -22,3 +22,19 @@ REM Reads
 %diskspd_path%\amd64\diskspd.exe -b64K -d30 -h -L -o16 -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o16.txt
 %diskspd_path%\amd64\diskspd.exe -b64K -d30 -h -L -o32 -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o32.txt
 
+REM Writes
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o1  -t%logical_processors% -w100 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o1.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o2  -t%logical_processors% -w100 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o2.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o4  -t%logical_processors% -w100 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o4.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o8  -t%logical_processors% -w100 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o8.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o16 -t%logical_processors% -w100 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o16.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o32 -t%logical_processors% -w100 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o32.txt
+
+REM Reads
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o1  -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o1.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o2  -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o2.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o4  -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o4.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o8  -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o8.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o16 -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o16.txt
+%diskspd_path%\amd64\diskspd.exe -b8K -d30 -h -L -o32 -t%logical_processors% -w0 -r -c10240M %test_path% > %mydocs%\Diskpdresult_read_64k_M_o32.txt
+
