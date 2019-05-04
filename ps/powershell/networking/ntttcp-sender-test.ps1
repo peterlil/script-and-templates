007abd4b16cff2d1b7a8f6d7435de2cbb5f8985a
Peter Liljenroth <sail@live.se>
2019-05-04 15:06:33 +0200

    ntttcp scripts

---
+++ ps/powershell/networking/ntttcp-sender-test.ps1
@@ -0,0 +1,24 @@
+# Sender
+$ReceiverIp = '10.2.0.5';
+$NtttcpPath = 'C:\Users\peterlil\Downloads\NTttcp-v5.33\amd64fre\ntttcp.exe'
+$NumberOfCores = 4;
+$ThreadsPerCore = 4;
+$Async = $true;
+
+
+$AsyncParam = "";
+if($Async) {
+    $AsyncParam = " -a 2";
+}
+
+for($iCore = 0; $iCore -lt $NumberOfCores; $iCore++) {
+    for($iThread = 0; $iThread -lt $ThreadsPerCore; $iThread++) {
+        $Threads = $iCore*$ThreadsPerCore + ($iThread + 1);
+        $Command = 
+@"
+cmd.exe /C $NtttcpPath -s -m $Threads,$iCore,$ReceiverIp$AsyncParam
+"@
+        $Command
+        Invoke-Expression -Command:$Command
+    }
+}
