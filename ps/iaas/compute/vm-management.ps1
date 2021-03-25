6d4fbcb8f6cc97c591ccd595fe9bdbb81f514db2
peterlil <peterlil@microsoft.com>
2021-03-25 09:30:41 +0100

    starting script for managing vms

---
+++ ps/iaas/compute/vm-management.ps1
@@ -0,0 +1,32 @@
+﻿#
+# Login to Azure
+#
+az login
+
+#
+# Check power state for each VM
+#
+az vm list -d -o table
+
+#
+# Check the power state for a particular vm.
+#
+az vm list -d -o table --query "[?name=='pljava']"
+
+#
+# Get a list of the running vms.
+#
+az vm list -d -o table --query "[?powerState=='VM running']"
+
+
+
+
+# Embryo's
+
+$vmlist = az vm list | ConvertFrom-Json
+
+$vmlist | ft name, provisioningState
+
+az vm list --help
+
+
