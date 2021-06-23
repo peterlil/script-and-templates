d82c43e2a6fd6fd59e7f67ae15909ad550b7e60d
peterlil <29121387+peterlil@users.noreply.github.com>
2021-06-23 19:12:03 +0200

    sql and iaas disk management

--- ps/windows/disk-management/disk-management.ps1
+++ ps/windows/disk-management/disk-management.ps1
@@ -1,12 +1,32 @@
 
 $diskDrives = Get-WmiObject -Class Win32_DiskDrive
-$diskDriveToDiskPartitionMapping = Get-WmiObject -Class Win32_DiskDriveToDiskPartition
-$logicalDiskToPartitionMapping = Get-WmiObject -Class Win32_LogicalDiskToPartition
-$logicalDisk = Get-WmiObject -Class Win32_LogicalDisk
+$diskDriveToDiskPartitionMappings = Get-WmiObject -Class Win32_DiskDriveToDiskPartition
+$logicalDiskToPartitionMappings = Get-WmiObject -Class Win32_LogicalDiskToPartition
+$logicalDisks = Get-WmiObject -Class Win32_LogicalDisk
 
-$diskDrive | ForEach-Object {
+
+$diskDrives | Format-Table __Path
+$diskDriveToDiskPartitionMappings | Format-Table Antecedent
+
+$diskDrives | ForEach-Object {
+  $diskDrive = $_
   if ($_)
   {
+  
+    #$logicalDiskToPartitionMapping = $logicalDiskToPartitionMappings | 
+    #  Where-Object {  }
+
     
+
+    $diskDriveToDiskPartitionMapping = $diskDriveToDiskPartitionMappings |
+      Where-Object { $_.Antecedent -eq $diskDrive.__Path }
+    
+    New-Object -TypeName PSObject -Property @{
+      Col1 = $diskDriveToDiskPartitionMapping.Antecedent
+      SCSIBus = $DiskDrive.SCSIBus
+      SCSIPort = $DiskDrive.SCSIPort
+      SCSITargetId = $DiskDrive.SCSITargetId
+      SCSILogicalUnit = $DiskDrive.SCSILogicalUnit
+    }
   }
 }
