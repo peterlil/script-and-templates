7617dd30cae6c8c71ef0e8be699d5900dfbec5f8
peterlil <29121387+peterlil@users.noreply.github.com>
2021-09-03 08:30:02 +0200

    Mande RequestJitAccess as a CmdLet

---
+++ az cli/iaas/compute/cmd-lets/RequestJitAccess.ps1
@@ -0,0 +1,138 @@
+################################################################################
+# Request just-in-time access
+# Inspiration taken from: 
+# - https://www.improvescripting.com/how-to-create-custom-powershell-cmdlet-step-by-step/
+# - https://wahlnetwork.com/2017/07/10/powershell-aliases/
+################################################################################
+
+#region help
+<#
+.SYNOPSIS
+Requests Just-in-time access to a virtual machine in Azure.
+.DESCRIPTION
+Requests Just-in-time access to a virtual machine in Azure.
+If the current session is not already logged in to Azure, it starts an interactive login process first. 
+
+.PARAMETER ResourceGroupName
+Name of the resource group containing the VM.
+
+.PARAMETER VirtualMachineName
+Name of the virtual machine
+
+.EXAMPLE
+Request-JitAccess -ResourceGroupName "myRg" -VirtualMachineName "myVm"
+
+.EXAMPLE
+Request-JitAccess -resource-group "myRg" -name "myVm"
+
+.EXAMPLE
+Request-JitAccess -g "myRg" -n "myVm"
+
+.INPUTS
+System.String
+
+InputObject parameters are strings. 
+.OUTPUTS
+Outputs to console.
+
+.NOTES
+FunctionName : Request-JitAccess
+Created by   : Peter Liljenroth
+Date Coded   : 09/03/2021
+More info    : https://github.com/peterlil/script-and-templates
+
+#>
+#endregion
+Function Request-JitAccess {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, 
+            HelpMessage="Name of the resource group for the VM.")]
+        [Alias('resource-group', 'g')]
+        [string]$ResourceGroupName,
+
+        [Parameter(Mandatory=$true, 
+            HelpMessage="Name of the VM.")]
+        [Alias('name', 'n')]
+        [string]$VirtualMachineName
+    )
+    BEGIN {
+        
+    }
+
+    PROCESS {
+        
+        # Check that the session is logged in
+        $subscriptions = az account list --output table
+        if($subscriptions.Length -lt 3) {
+            Write-Verbose "Session is not logged in to Azure. Logging in."
+            $subscriptions = az login --output table
+            if($subscriptions.Length -lt 3) {
+                Write-Error "Log in error or user has no subscription access. Terminating."
+                return ""
+            }
+        }
+
+        Write-Verbose "Session is logged in, looking for resource group."
+        # Check that the resource group exists
+        $groups = az group list --query "[?name=='$ResourceGroupName']" -o table
+        if($groups.Length -eq 0 ) {
+            Write-Error "The resource group $ResourceGroupName does not exist."
+            return ""
+        }
+        
+        Write-Verbose "Resource group $ResourceGroupName exists, looking for VM."
+        # Check that the VM exists
+        $vms = az vm list -g $ResourceGroupName --query "[?name=='$VirtualMachineName']" -o table
+        if($vms.Length -eq 0) {
+            Write-Error "The virtual machine $VirtualMachineName does not exist."
+            return ""
+        }
+        Write-Verbose "Virtual machine  $VirtualMachineName exists, proceeding."
+
+        $ip = (Invoke-WebRequest -Uri https://ifconfig.me/ip | Select-Object Content).Content
+        Write-Verbose "Local ip: $ip"
+        $vmInfo = (az vm show -g $ResourceGroupName -n $VirtualMachineName -o tsv --query "[id, location]")
+        # Reg exp to find a GUID: (?<SubscriptionId>[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})
+        $null = $vmInfo[0] -match "(?<SubscriptionId>[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})"
+        Write-Verbose "Subscription ID: $($matches.SubscriptionId)"
+        $requestName = New-Guid
+
+        $EndPoint = "https://management.azure.com/subscriptions/$($matches.SubscriptionId)/resourceGroups/$ResourceGroupName/providers/Microsoft.Security/locations/$($vmInfo[1])/jitNetworkAccessPolicies/default/initiate?api-version=2015-06-01-preview"
+        $Body = "{""requests"":[{""content"":{""virtualMachines"":[{""id"":""$($vmInfo[0])"",""ports"":[{""number"":3389,""duration"":""PT10H"",""allowedSourceAddressPrefix"":""$ip""}]}]},""httpMethod"":""POST"",""name"":""$requestName"",""requestHeaderDetails"":{""commandName"":""Microsoft_Azure_Compute.""},""url"":""$EndPoint""}]}" | ConvertTo-Json
+        $Url="https://management.azure.com/batch?api-version=2020-06-01"
+
+        Write-Verbose "Endpoint: $EndPoint"
+        Write-Verbose "Body: $Body"
+        Write-Verbose "Uri: $Url"
+        
+        # Send the jit-request
+        $response = az rest --method post --uri "$Url" --body "$Body" --query "responses[0].[httpStatusCode,content.virtualMachines[0].ports[0].status]" -o table
+        
+        if($response[2] -eq "202") {
+            Write-Verbose "Request status: $($response[3])"
+        }
+
+        # Construct the request to check status.While a request is in 'Initiating' status, the request is still being applied.
+        $requestName = New-Guid
+        $Body2 = "{""requests"":[{""httpMethod"":""GET"",""name"":""$requestName"",""requestHeaderDetails"":{""commandName"":""Microsoft_Azure_Compute.""},""url"":""https://management.azure.com/subscriptions/$($matches.SubscriptionId)/resourceGroups/$ResourceGroupName/providers/Microsoft.Security/locations/$($vmInfo[1])/jitNetworkAccessPolicies/default?api-version=2015-06-01-preview""}]}" | ConvertTo-Json
+        $response = az rest --method post --uri "$Url" --body "$Body2" --query "responses[].content.properties.requests[?virtualMachines[?ports[?status=='Initiating']]].startTimeUtc" | ConvertFrom-Json
+        
+        while($response.Length -gt 0) {
+            Start-Sleep -Seconds 2
+            $response = az rest --method post --uri "$Url" --body "$Body2" --query "responses[].content.properties.requests[?virtualMachines[?ports[?status=='Initiating']]].startTimeUtc" | ConvertFrom-Json
+        }
+
+        $response = az rest --method post --uri "$Url" --body "$Body2" --query "responses[].content.properties.requests[?virtualMachines[?ports[?status=='Initiated']]].startTimeUtc" | ConvertFrom-Json
+        if( $response.Length -eq 0) {
+            Write-Error "Something went wrong"
+        }
+        else {
+            Write-Host "Jit request is granted."
+        }
+    }
+
+    END {
+
+    }
+}
