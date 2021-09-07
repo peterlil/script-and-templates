ab4b1af61e1f88adc6d48726234f0db9d3254f85
peterlil <29121387+peterlil@users.noreply.github.com>
2021-09-07 08:35:49 +0200

    .

---
+++ ps/powershell/Get-StringFromCsv.ps1
@@ -0,0 +1,22 @@
+﻿Function Get-PasswordFromCsv {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, 
+            HelpMessage="Path to the file")]
+        [string]$Path,
+
+        [Parameter(Mandatory=$true, 
+            HelpMessage="Vaule of the key")]
+        [string]$Key
+    )
+
+    $table = Import-Csv -Path $Path
+
+    $table | ForEach-Object {
+        if ($_.vm.ToLower() -eq $Key.ToLower()) {
+            Write-Host $_.pwd
+            break
+        }
+    }
+}
+
