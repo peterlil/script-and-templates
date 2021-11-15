ae503557184dcbcfbe0a9265865fdd35412ae68f
peterlil <29121387+peterlil@users.noreply.github.com>
2021-11-15 09:27:53 +0100

    disable/enable gtx. get key vault secrets

---
+++ az cli/key-vault/misc-keyvault-scripts.ps1
@@ -0,0 +1,10 @@
+﻿az keyvault secret list --subscription <subscription> --vault-name devboxes-vm-encrypt
+
+$value = az keyvault secret show -n swevm `
+    --subscription <subscription> `
+    --vault-name devboxes-vm-encrypt `
+    --query "value" `
+    -o tsv
+
+Set-Clipboard -Value $value
+
---
+++ ps/windows/disable-enable-gpu-for-detach-of-display.ps1
@@ -0,0 +1,11 @@
+﻿$GtxId = (Get-PnpDevice -FriendlyName "*GTX*").InstanceId
+echo "  DeviceID = $GtxId"
+
+echo "Disabling GTX."
+Disable-PnpDevice -Confirm:$false -InstanceId $GtxId
+
+# Wait for user to click detach button
+Read-Host -Prompt "Safe to detach. Try detach button now. Hit enter to re-enable GTX." 
+
+echo "Enabling GTX."
+Enable-PnpDevice -Confirm:$false -InstanceId $GtxId
