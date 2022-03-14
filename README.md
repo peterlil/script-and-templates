9af1be1e180853d9293127084038a3b1f9c9d7b1
Peter Liljenroth <29121387+peterlil@users.noreply.github.com>
2022-03-14 11:53:36 +0100

    .

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Here I keep misc scripts and templates for Azure development and resource provis
 ### Sample solution for using Application Gateway with a WebApp and WebApi
 
 [Go here]() to find an example of how to secure a webapp and a webapi with Application Gateway.
+
+adfas
+dfas
--- ps/powershell/powershell-profile-2022-02-10.ps1
+++ ps/powershell/powershell-profile-2022-02-10.ps1
@@ -269,3 +269,17 @@ Function Stop-VM {
     az vm deallocate -g $rgName -n $name
 
 }
+
+Function DisableEnable-GPU {
+    $GtxId = (Get-PnpDevice -FriendlyName "*GTX*").InstanceId
+    echo "  DeviceID = $GtxId"
+
+    echo "Disabling GTX."
+    Disable-PnpDevice -Confirm:$false -InstanceId $GtxId
+
+    # Wait for user to click detach button
+    Read-Host -Prompt "Safe to detach. Try detach button now. Hit enter to re-enable GTX." 
+
+    echo "Enabling GTX."
+    Enable-PnpDevice -Confirm:$false -InstanceId $GtxId
+}
