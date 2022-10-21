3e2e9ca563071e1293ee192708c0809a07c23cdc
peterlil <29121387+peterlil@users.noreply.github.com>
2022-10-21 11:30:35 +0200

    .

--- azure-services/identity/obo-flow-sample/obo-web-client/Program.cs
+++ azure-services/identity/obo-flow-sample/obo-web-client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Azure.Identity;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
@@ -12,14 +13,43 @@ string[] scopes = builder.Configuration.GetValue<string>("obo-api-server-sample:
 // Add services to the container.
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"), "OpenIdConnect", "Cookies", true)
-    .EnableTokenAcquisitionToCallDownstreamApi(scopes)
-    .AddInMemoryTokenCaches();
+        .EnableTokenAcquisitionToCallDownstreamApi(scopes)
+            .AddInMemoryTokenCaches();
+
+string appConfigCnStr = builder.Configuration.GetConnectionString("AppConfig");
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(appConfigCnStr)
+        .ConfigureKeyVault(async kv =>
+        {
+            /*
+            var o = new DefaultAzureCredentialOptions();
+            o.SharedTokenCacheUsername = "peterlil@mngenv319828.onmicrosoft.com";
+            kv.SetCredential(new DefaultAzureCredential(o));
+            */
+            kv.SetCredential(new DefaultAzureCredential());
+        });
+});
+
+/*
+https://stackoverflow.com/questions/61524182/msal-net-no-account-or-login-hint-was-passed-to-the-acquiretokensilent-call
+https://stackoverflow.com/questions/62518766/error-no-account-or-login-hint-was-passed-to-the-acquiretokensilent-call
+https://stackoverflow.com/questions/60524263/account-not-found-after-restart-no-account-or-login-hint-was-passed-to-the-acqu
+https://learn.microsoft.com/en-us/azure/active-directory/develop/msal-net-token-cache-serialization?tabs=aspnetcore
+https://learn.microsoft.com/en-us/azure/azure-app-configuration/quickstart-aspnet-core-app?tabs=core6x
+https://www.youtube.com/watch?v=TU82BTmeNeU
+https://localhost:7286/
+https://learn.microsoft.com/en-us/answers/questions/714417/unable-to-retrieve-password-from-keyvault-error-ak.html
+https://github.com/Azure/azure-cli/issues/11871
+https://www.rahulpnath.com/blog/defaultazurecredential-from-azure-sdk/
+*/
 
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
+
 builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
 
