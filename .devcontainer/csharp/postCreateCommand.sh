4ad02e9d2c8047f61c5e2b40dd938a794568ec20
Peter Liljenroth <29121387+peterlil@users.noreply.github.com>
2022-11-09 21:06:56 +0100

    Create postCreateCommand.sh

---
+++ .devcontainer/csharp/postCreateCommand.sh
@@ -0,0 +1,5 @@
+sudo wget https://github.com/jmespath/jp/releases/latest/download/jp-linux-amd64 \
+  -O /usr/local/bin/jp  && sudo chmod +x /usr/local/bin/jp
+  
+sudo apt-get update
+sudo apt-get install uuid-runtime
